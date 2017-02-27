9375808a13316e2d62dc9a6f30f770090898c13a
Joseph Frazier <1212jtraceur@gmail.com>
2017-02-27 11:38:31 -0500

    Ensure there's a space before the permalink

--- test/main-lib.js
+++ test/main-lib.js
@@ -7,14 +7,15 @@ require('dotenv-safe').config();
 
 test('permalinker', (t) => {
   t.test('https://github.com/isaacs/github/issues/625#issuecomment-203464167', (t) => {
-    t.plan(2);
+    t.plan(3);
 
     jsdom.env('https://github.com/isaacs/github/issues/625#issuecomment-203464167', (err, { document }) => {
       t.ifError(err);
 
       permalink({ token: process.env.GITHUB_TOKEN }, document).then(() => {
-        t.equal(document.querySelector('[href="https://github.com/andrewthad/yesod-table/blob/master/src/Yesod/Table.hs"]').nextElementSibling.href,
-          'https://github.com/andrewthad/yesod-table/blob/57a2b5b385612d67f76f19d5c6164e182dee4fcf/src/Yesod/Table.hs');
+        const fragileLink = document.querySelector('[href="https://github.com/andrewthad/yesod-table/blob/master/src/Yesod/Table.hs"]');
+        t.equal(fragileLink.nextSibling.textContent.length, 2); // can't seem to check for ' ('
+        t.equal(fragileLink.nextElementSibling.href, 'https://github.com/andrewthad/yesod-table/blob/57a2b5b385612d67f76f19d5c6164e182dee4fcf/src/Yesod/Table.hs');
       });
     });
   });
