1e3129578b70f55d9a3d026e68c05a1cfb133110
Joseph Frazier <1212jtraceur@gmail.com>
2017-02-26 22:11:54 -0500

    Replace karma/mocha stuff with tape tests

--- test/main-lib.js
+++ test/main-lib.js
@@ -1,1 +1,19 @@
-// TODO
+/* eslint-disable id-length, no-shadow */
+import test from 'tape';
+import jsdom from 'jsdom';
+import permalink from '../lib/permalinker';
+
+test('permalinker', (t) => {
+  t.test('https://github.com/isaacs/github/issues/625#issuecomment-203464167', (t) => {
+    t.plan(2);
+
+    jsdom.env('https://github.com/isaacs/github/issues/625#issuecomment-203464167', (err, window) => {
+      t.ifError(err);
+
+      permalink({}, window).then(() => {
+        t.equal(window.document.querySelector('[href="https://github.com/andrewthad/yesod-table/blob/master/src/Yesod/Table.hs"]').nextElementSibling.href,
+          'https://github.com/andrewthad/yesod-table/blob/57a2b5b385612d67f76f19d5c6164e182dee4fcf/src/Yesod/Table.hs');
+      });
+    });
+  });
+});
