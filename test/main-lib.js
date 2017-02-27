3f28211fccc0e47c7763bd254df73dba90d14087
Joseph Frazier <1212jtraceur@gmail.com>
2017-02-27 02:40:12 -0500

    Use github token in tests
    Note that for local usage, it should be stored in a .env file like
    
        GITHUB_TOKEN=yourtoken
    
    See here for CI details:
    
    * https://docs.travis-ci.com/user/environment-variables/#Defining-encrypted-variables-in-.travis.yml
    * https://www.appveyor.com/docs/build-configuration/#secure-variables

--- test/main-lib.js
+++ test/main-lib.js
@@ -3,6 +3,8 @@ import test from 'tape';
 import jsdom from 'jsdom';
 import permalink from '../lib/permalinker';
 
+require('dotenv').config();
+
 test('permalinker', (t) => {
   t.test('https://github.com/isaacs/github/issues/625#issuecomment-203464167', (t) => {
     t.plan(2);
@@ -10,7 +12,7 @@ test('permalinker', (t) => {
     jsdom.env('https://github.com/isaacs/github/issues/625#issuecomment-203464167', (err, { document }) => {
       t.ifError(err);
 
-      permalink({}, document).then(() => {
+      permalink({ token: process.env.GITHUB_TOKEN }, document).then(() => {
         t.equal(document.querySelector('[href="https://github.com/andrewthad/yesod-table/blob/master/src/Yesod/Table.hs"]').nextElementSibling.href,
           'https://github.com/andrewthad/yesod-table/blob/57a2b5b385612d67f76f19d5c6164e182dee4fcf/src/Yesod/Table.hs');
       });
